36b64ee34ec8645fd0ba56b76c81e5a2cd6f3f25
lucas10100 <lucasdolores294@gmail.com>
2023-03-15 11:25:26 -0300

    add

--- py.py
+++ py.py
@@ -2,7 +2,6 @@
 n = int(input("digite a sequencia ? "))
 t1 = 0 
 t2= 1
-
 print('{} > {}'.format(t1,t2), end="")
 cont = 3
 while cont <= n:
@@ -11,3 +10,11 @@ while cont <= n:
   t1 = t2 
   t2= t3
   cont += 1
+  print("-")
+if n== t1 or n==t2:
+      print("faz parte ")
+elif n==t3:
+       print("faz parte")
+else:
+    print("não faz parte")
+       
