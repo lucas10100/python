f5d1924920b660d4c5500d4730ae0bf9a4ae7f24
lucas10100 <lucasdolores294@gmail.com>
2023-03-15 11:48:59 -0300

    add

--- py.py
+++ py.py
@@ -1,20 +1,2 @@
-
-n = int(input("digite a sequencia ? "))
-t1 = 0 
-t2= 1
-print('{} > {}'.format(t1,t2), end="")
-cont = 3
-while cont <= n:
-  t3= t1 + t2
-  print(' > {}'.format(t3), end="")
-  t1 = t2 
-  t2= t3
-  cont += 1
-  print("-")
-if n== t1 or n==t2:
-      print("faz parte ")
-elif n==t3:
-       print("faz parte")
-else:
-    print("não faz parte")
-       
+txt = input("digite a palavra ou frase a ser invertidada ")
+print(txt[::-1])
