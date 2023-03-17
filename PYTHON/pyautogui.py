514182a1800376a2568cc6aa6af10e1e40daa637
lucas10100 <lucasdolores294@gmail.com>
2023-03-17 18:32:04 -0300

    atualizando

--- PYTHON/pyautogui.py
+++ PYTHON/pyautogui.py
@@ -1,19 +1,17 @@
 import pyautogui
-import time
-
-pyautogui.PAUSE=1
 pyautogui.press(['win'])
 
-pyautogui.write("google")
-pyautogui.press(['enter'])
-pyautogui.write("whatsapp web")
-time.sleep(3)
-pyautogui.press(['enter'])
-time.sleep(3)
-pyautogui.click(x=331, y=636)
-pyautogui.press(['enter'])                           
-pyautogui.click(x=239, y=324)
-pyautogui.press(['enter'])  
-time.sleep(3)
 
-pyautogui.press(['enter'])
+# pyautogui.write("google")
+# pyautogui.press(['enter'])
+# pyautogui.write("whatsapp web")
+# time.sleep(3)
+# pyautogui.press(['enter'])
+# time.sleep(3)
+# pyautogui.click(x=331, y=636)
+# pyautogui.press(['enter'])                           
+# pyautogui.click(x=239, y=324)
+# pyautogui.press(['enter'])  
+# time.sleep(3)
+
+# pyautogui.press(['enter'])
--- py/analise.py
+++
@@ -1,4 +0,0 @@
-import pandas 
-
-tabela = pandas.read_csv("clientes.csv" encoding="latim")
-print(tabela)
