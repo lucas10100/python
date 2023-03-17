556047ab86bab764ad4f264fd13cbbe75a74dadc
lucas10100 <lucasdolores294@gmail.com>
2023-03-17 20:02:23 -0300

    add

---
+++ PYTHON/automação python/automaçao.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+pyautogui.PAUSE=1
+pyautogui.press(['win'])
+pyautogui.write('google')
+pyautogui.press(['enter'])
+pyautogui.write('gmail')
+pyautogui.press(['enter'])
+time.sleep(4)
+pyautogui.click(x=245, y=624)
+pyautogui.press(['enter'])
+pyautogui.click(x=191, y=325)
+pyautogui.press(['enter'])
+time.sleep(5)
+pyautogui.click(x=72, y=195)
+pyautogui.press(['enter'])
+pyautogui.write('lucasdolores293@hotmail.com')
+pyautogui.press(['enter'])
+pyautogui.press(['tab'])
+pyautogui.press(['tab'])
+pyautogui.click(x=962, y=688) #anexo
+pyautogui.press(['enter'])
+pyautogui.click(x=93, y=123) #area de trabalho 
+pyautogui.press(['enter'])
+pyautogui.click(x=269, y=162)
+pyautogui.press(['enter'])
+pyautogui.click(x=279, y=140)
+pyautogui.press(['enter'])
+pyautogui.click(x=231, y=143)
+pyautogui.press(['enter'])
+pyautogui.click(x=835, y=695)
+pyautogui.press(['enter'])
+#pyautogui.click(x=754, y=164)
